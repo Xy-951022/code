23f7cd82bcfb8fbc42fce1973e9e427be2d6a29e
xy <1773758461@qq.com>
2019-06-12 16:48:07 +0800

    提交

---
+++ git/src/cn/itcast/Demo.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
